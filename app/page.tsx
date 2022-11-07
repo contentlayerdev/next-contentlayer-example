83bc71dbae43f55dc27dab707bdc0f4ea2553e25
Johannes Schickling <schickling.j@gmail.com>
2022-11-07 17:23:36 +0100

    WIP RSC

--- app/page.tsx
+++ app/page.tsx
@@ -2,21 +2,11 @@ import Link from 'next/link'
 import { compareDesc, format, parseISO } from 'date-fns'
 import { allPosts, Post } from 'contentlayer/generated'
 
-export async function getStaticProps() {
-  const posts: Post[] = allPosts.sort((a, b) => {
-    return compareDesc(new Date(a.date), new Date(b.date))
-  })
-  return { props: { posts } }
-}
-
 function PostCard(post: Post) {
   return (
     <div className="mb-8">
       <h2 className="text-xl">
-        <Link
-          href={post.url}
-          className="text-blue-700 hover:text-blue-900"
-          legacyBehavior>
+        <Link href={post.url} className="text-blue-700 hover:text-blue-900" legacyBehavior>
           {post.title}
         </Link>
       </h2>
@@ -25,10 +15,14 @@ function PostCard(post: Post) {
       </time>
       <div className="text-sm" dangerouslySetInnerHTML={{ __html: post.body.html }} />
     </div>
-  );
+  )
 }
 
-export default function Home({ posts }: { posts: Post[] }) {
+export default function Home() {
+  const posts: Post[] = allPosts.sort((a, b) => {
+    return compareDesc(new Date(a.date), new Date(b.date))
+  })
+
   return (
     <div className="max-w-xl mx-auto py-8">
       <h1 className="text-3xl font-bold mb-8 text-center">Next.js Example</h1>
